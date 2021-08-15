f3b1d298ede10690ea3bccc2f1eb651570441e2c
codeking5 <mayanksmind@gmail.com>
2021-08-15 19:23:30 +0530

    Under Construction

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use app\controllers\SiteContorller;
+use app\core\Application;
+
+
+$app = new Application(dirname(__DIR__));
+
+$app->router->get("/","home");
+
+$app->router->get("/contact","contact");
+$app->router->post("/contact",[SiteContorller::class,'handleContact']);
+
+$app->run();
