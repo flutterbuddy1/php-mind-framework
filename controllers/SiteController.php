d481d1df44291ac85ad8096be3d9d9c5a4de9486
codeking5 <mayanksmind@gmail.com>
2021-08-15 19:19:15 +0530

    Under Construction

---
+++ controllers/SiteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+
+class SiteContorller{
+
+    public function contact(){
+        return "Handling Submitted Data";
+    }
+
+    public function handleContact(){
+        return "contact";
+    }
+}
---
+++ views/home.mind.php
@@ -0,0 +1,1 @@
+<h1 class="text-danger">Home Page</h1>
